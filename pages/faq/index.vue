8931c54ba74a1f83b8ea547ea76146c6f965a4ff
Junscana <junscana@gmail.com>
2021-07-01 18:46:46 +0900

    【Faq】Fix：FAQ情報修正、追加

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -24,7 +24,7 @@
               @before-leave="beforeLeave"
               @leave="leave"
             >
-              <p v-show="isOpen[index]" v-text="item.content" class="accordion__inner__item__content topSlide" />
+            <p v-show="isOpen[index]" class="accordion__inner__item__content topSlide">{{ item.content }}<span v-if="item.hasTextLink" class="text-link"><nuxt-link :to="item.textLink">{{ item.linkText }}</nuxt-link></span></p>
             </transition>
           </li>
         </ul>
@@ -70,30 +70,52 @@ export default {
     getContents () {
       const data = [
         {
-          title: 'ここに質問が入ります',
-          content: 'ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。'
+          title: 'プレゼントはできますか？',
+          content: 'プレゼントは事前にプレゼント可能の告知が出ている定期公演、主催ライブのみ可能です。メンバーへの直接のお渡しはお断りしております。物販スタッフまでお渡しください。プレゼントできないものもございますので、［物販・特典会レギュレーションとイベント参加に関する注意事項］を合わせてご確認ください。',
+          hasTextLink: true,
+          textLink: '/information/',
+          linkText: '［物販・特典会レギュレーションとイベント参加に関する注意事項］'
         },
         {
-          title: 'ここに質問が入ります',
-          content: 'ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。'
+          title: 'チケットの購入方法は？',
+          content: 'ライブごとに異なります。\nOVERFLOW公式Twitterにてライブ予定とチケット購入リンクを随時告知しております。またOVERFLOW公式ホームページのスケジュールページからも、ライブごとに購入リンクを記載しております。そちらもご参考ください。',
+          hasTextLink: false
         },
         {
-          title: 'ここに質問が入ります',
-          content: 'ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。ここに回答が入ります。'
+          title: 'ライブのルールは？',
+          content: '会場やイベントによりルールが異なります。それぞれのルールに従ってご参加ください。また、OVERFLOWの観覧ルールなどは［物販・特典会レギュレーションとイベント参加に関する注意事項］のページにまとめておりますのでご確認ください。',
+          hasTextLink: true,
+          textLink: '/information/',
+          linkText: '［物販・特典会レギュレーションとイベント参加に関する注意事項］'
+        },
+        {
+          title: 'ライブは撮影できますか？',
+          content: '原則撮影は禁止です。撮影可能の際は公式Twitterより告知します。',
+          hasTextLink: false
+        },
+        {
+          title: '特典会の参加方法は？',
+          content: '①参加には特典券が必要です。(a),(b)どちらの方法でも入手できます。\n(a)チェキ券を購入する→物販購入列へお並びください。グッズもこちらで購入できます。\n(b)グッズ特典券で参加する→グッズ購入2000円毎にお渡しする『グッズ購入特典券』を集めて特典会に参加することもできます。(1枚→写メ、2枚→サインなしチェキ、3枚→サイン有りチェキ、4枚→30秒動画or集合チェキ)\n\n②特典会列に並び、撮影。\n・1度に複数枚券を出すことはできません。\n1枚使用後に特典会列に並び直しをお願いします。\n・チェキ、グッズ購入特典券、入場特典券など、どの種類の券も同じ列にお並びください。\n・チェキ、写メ共にメンバーのみでの撮影も可能です。\n・その他［物販・特典会レギュレーションとイベント参加に関する注意事項］をご確認ください。',
+          hasTextLink: true,
+          textLink: '/information/',
+          linkText: '［物販・特典会レギュレーションとイベント参加に関する注意事項］'
+        },
+        {
+          title: 'スケジュールはいつ更新されますか？',
+          content: 'スケジュール更新は随時変更、追加します。予告なしに変更、追加がある場合や、Twitterの告知と前後する場合もございますので、予めご了承ください。',
+          hasTextLink: false
         }
       ]
       return data
     }
   },
   methods: {
-    // メニューを開閉する
     handleToggle (index) {
       if (!this.isOpen[index]) {
         this.isOpen = Array(this.getContents.length).fill(false)
       }
       this.isOpen.splice(index, 1, !this.isOpen[index])
     },
-    // スライド開閉要素の高さ取得
     beforeEnter (el) {
       el.style.height = '0'
     },
@@ -111,7 +133,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.accordion__inner__item{
+.accordion__inner__item {
   margin: 0 1.5rem;
   border-top: 1px solid #6AC0F0;
   box-sizing: border-box;
@@ -166,9 +188,9 @@ export default {
 .accordion__inner__item__content {
   padding: 1.5rem 0 0;
   border-top: 1px solid #6AC0F0;
-
   transition: height 0.3s ease-in-out;
   overflow: hidden;
+  white-space: pre-wrap;
 }
 
 .topSlide {
@@ -186,9 +208,22 @@ export default {
   animation-fill-mode: both;
 }
 
-@media screen and (min-width: 768px) {
+.text-link {
+  display: block;
+  margin-top: 1rem;
+
+  a {
+    color: #0066c0;
+  }
+}
+
+@media screen and (min-width: 769px) {
   .page-body {
     max-width: 68rem;
+  }
+
+  .accordion__inner__item {
+    margin: 0;
   }
 }
 </style>
